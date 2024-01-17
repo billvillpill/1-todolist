eb4ea3a13e0a0965b63da9eadd7034560c1f5e08
billvillpill <belarus0000@gmail.com>
2024-01-17 20:43:56 +0300

    переделал все button and input on material ui, hover on delete button, рефакторинг, useReducer for todolists

--- src/components/EditableSpan.tsx
+++ src/components/EditableSpan.tsx
@@ -1,6 +1,6 @@
 import React, {ChangeEvent, useState} from 'react';
 import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
+
 
 type EditableSpanProps = {
     oldTitle: string
